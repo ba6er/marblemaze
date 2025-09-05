ecbaac0127a8b2d705ec2a783de1d82711959d0a
Ba6er <ba6er@tutamail.com>
2025-09-05 12:56:03 +0200

    Added ren::Camera implementation

---
+++ src/ren/camera.cpp
@@ -0,0 +1,38 @@
+#include <ren/camera.hpp>
+
+using namespace ren;
+
+void Camera::project2d(float left, float right, float bottom, float top, float near, float far) {
+	projection = lin::Mat4::Project2d(left, right, bottom, top, near, far);
+}
+
+void Camera::project3d(float fov, float aspect, float near, float far) {
+	projection = lin::Mat4::Project3d(fov, aspect, near, far);
+}
+
+void Camera::setPosition(lin::Vec3 pos) {
+	this->pos = pos;
+	updateViewMatrix();
+}
+
+void Camera::setTarget(lin::Vec3 target) {
+	this->target = target;
+	updateViewMatrix();
+}
+
+void Camera::setUp(lin::Vec3 up) {
+	this->up = up;
+	updateViewMatrix();
+}
+
+lin::Mat4 Camera::getViewMatrix() {
+	return view;
+}
+
+lin::Mat4 Camera::getProjectionMatrix() {
+	return projection;
+}
+
+void Camera::updateViewMatrix() {
+	view = lin::Mat4::LookAt(pos, target, up);
+}
---
+++ src/ren/camera.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <lin/linalg.hpp>
+
+namespace ren {
+
+	class Camera {
+
+	public:
+		void project2d(float left, float right, float bottom, float top, float near, float far);
+		void project3d(float fov, float aspect, float near, float far);
+
+		void setPosition(lin::Vec3 pos);
+		void setTarget(lin::Vec3 target);
+		void setUp(lin::Vec3 up);
+
+		lin::Mat4 getViewMatrix();
+		lin::Mat4 getProjectionMatrix();
+
+	private:
+		void updateViewMatrix();
+
+		lin::Vec3 pos, target, up;
+		lin::Mat4 view, projection;
+	};
+}
